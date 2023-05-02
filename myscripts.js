e94f399007d0bd3da92476eaccaa3c1c4b26e1d0
Sanjog Wanem <sjwanem@gmail.com>
2023-05-02 19:59:24 +0100

    02/05/2023

--- myscripts.js
+++ myscripts.js
@@ -3,3 +3,8 @@ function myFunction ()
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+//  Scrolls the website to the top everytime it refreshes
+window.onload = function() {
+    window.scrollTo(0, 0);
+}
